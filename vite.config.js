86b0c6d8333d4f637c0d23b8b8e0f96cae32d87f
Dr Christopher Gerteis <christopher.gerteis@gmail.com>
2025-12-27 10:08:01 +0000

    Enhance Vite config with build options
    Added build configuration with multiple entry points.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: './',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        references: resolve(__dirname, "references.html"),
+        maritime: resolve(__dirname, "maritime-corridor.html"),
+      },
+    },
+  },
+});
